475e8e1884f71299f48e49d6f48238cbaaf60adc
David Alejandro De los Reyes Ostos <david.delosreyes@estudiantesunibague.edu.co>
2025-10-23 01:44:22 +0000

    Updated database.py

--- app/database.py
+++ app/database.py
@@ -1,14 +1,28 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
-from dotenv import load_dotenv
-
+#from dotenv import load_dotenv
+import ssl
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+# Detectar si estamos en Azure
+def is_azure_environment():
+    return DATABASE_URL and "postgres.database.azure.com" in DATABASE_URL
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+if is_azure_environment():
+    # Configuración para Azure PostgreSQL con SSL
+    engine = create_async_engine(
+        DATABASE_URL,
+        echo=True,
+        connect_args={
+            "ssl": ssl.create_default_context(cafile="/etc/ssl/certs/ca-certificates.crt")
+        }
+    )
+else:
+    # Configuración Local
+    engine = create_async_engine(DATABASE_URL, echo=True)
 
 SessionLocal = sessionmaker(
     bind=engine,
